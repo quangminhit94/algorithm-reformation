d48b54a82ca3d9ce147e3805f74d0b9a94dfd4de
Quang Minh <quangminhit94@gmail.com>
2022-05-31 17:59:47 +0700

    modify

--- Excercise66.c
+++ Excercise66.c
@@ -2,8 +2,7 @@
 #include <math.h>
 void findX(int a, int b, int c)
 {
-	double x = 0;
-	printf("%dx^2 + %dx + %d = 0\n", a, b, c);
+	printf("%dx^4 + %dx^2 + %d = 0\n", a, b, c);
 	if (a == 0)
 	{
 		if (b == 0)
@@ -11,22 +10,31 @@ void findX(int a, int b, int c)
 			printf("x has many result\n");
 			return;
 		}
-		x = c * -1.0 / b;
-		printf("x = %lf\n", x);
+		double x1 = - sqrt(1.0 * c / b);
+		double x2 = -x1;
+		printf("x1 = %lf\n", x1);
+		printf("x2 = %lf\n", x2);
 		return;
 	}
 	double delta = b * b - 4.0 * a * c;
-	double x1 = 0, x2 = 0;
 	if(delta == 0) {
-		x = - b / 2.0 * a;
-		printf("x = %lf\n", x);
+		double x1 = - sqrt(b / 2.0 * a);
+		double x2 = -x1;
+		printf("x1 = %lf\n", x1);
+		printf("x2 = %lf\n", x2);
 		return;
 	}
 	if (delta > 0)
 	{
-		x1 = (-b + sqrt(delta)) / 2.0 * a;
-		x2 = (-b - sqrt(delta)) / 2.0 * a;
-		printf("x1 = %lf and x2 = %lf\n", x1, x2);
+		double x1 = 0, x2 = 0, x3 = 0, x4 = 0;
+		x1 = - sqrt((b - sqrt(delta)) / 2.0 * a);
+		x2 = - x1;
+		x3 = - sqrt(b + sqrt(delta)) / 2.0 * a;
+		x4 = - x3;
+		printf("x1 = %lf\n", x1);
+		printf("x2 = %lf\n", x2);
+		printf("x3 = %lf\n", x3);
+		printf("x4 = %lf\n", x4);
 		return;
 	}
 	if (delta < 0)
